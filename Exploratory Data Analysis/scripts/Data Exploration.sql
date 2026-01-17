95a91db47d442f31dc6f0afdb2dc0513f028fecc
isaac-ik <isaacikhaiduwor1@gmail.com>
2026-01-17 06:20:06 -0500

    How many customers by country, gender, and marital_status

--- Exploratory Data Analysis/scripts/Data Exploration.sql
+++ Exploratory Data Analysis/scripts/Data Exploration.sql
@@ -1,23 +1,55 @@
-SELECT COUNT(*) as Number_of_products
-FROM gold.dim_products;
-
-CREATE VIEW NotBought AS(
-SELECT p.product_id, p.product_name
+--Are there products in dim_products that never appear in fact_sales?
+SELECT COUNT(p.product_key) AS Number_of_Unsold_categories
 FROM gold.dim_products p
 LEFT JOIN gold.fact_sales s
-ON p.product_id = s.product_key);
-
-SELECT COUNT(*)
-FROM NotBought;
-
-SELECT DISTINCT f.product_key
-FROM gold.fact_sales f;
-
-SELECT *
-FROM gold.dim_products p;
-
-SELECT DISTINCT p.product_id, s.product_key
-FROM gold.dim_products p
-LEFT JOIN gold.fact_sales s
-ON p.product_id = s.product_key
+ON p.product_key = s.product_key
 WHERE s.product_key IS NULL;
+
+--How many customers by country, gender, and marital_status?
+SELECT country, COUNT(*)
+FROM gold.dim_customers
+WHERE country != 'n/a'
+GROUP BY country;
+
+SELECT gender, COUNT(*)
+FROM gold.dim_customers
+WHERE gender != 'n/a'
+GROUP BY gender;
+
+SELECT marital_status, COUNT(*)
+FROM gold.dim_customers
+GROUP BY marital_status;
+
+---Combine into one table 
+SELECT 
+	'Country' AS Dimension, country, COUNT(*)
+FROM 
+	gold.dim_customers
+WHERE 
+	country != 'n/a'
+GROUP BY 
+	country
+UNION ALL
+SELECT 
+	'Gender' AS Dimension, gender, COUNT(*)
+FROM 
+	gold.dim_customers
+WHERE 
+	gender != 'n/a'
+GROUP BY 
+	gender
+UNION ALL
+SELECT 
+	'Martial Status' AS Dimension, marital_status, COUNT(*)
+FROM 
+	gold.dim_customers
+GROUP BY 
+	marital_status;
+
+
+
+--How many products per category, subcategory, and product_line?
+
+
+
+--Are some product lines under-represented in sales?
