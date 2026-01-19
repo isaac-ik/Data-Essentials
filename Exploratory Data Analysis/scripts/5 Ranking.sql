19dd6b8707720b80bd8cc2b86ed048bb4ad36b49
isaac-ik <isaac.ikpemi@gmail.com>
2026-01-19 13:26:49 -0500

    Bottom 10 products by quantity sold

--- Exploratory Data Analysis/scripts/5 Ranking.sql
+++ Exploratory Data Analysis/scripts/5 Ranking.sql
@@ -6,7 +6,22 @@ ON f.customer_key = c.customer_key
 GROUP BY f.customer_key, c.first_name, c.last_name
 ORDER BY TotalSales DESC
 LIMIT 10;
+
 -- Bottom 10 products by quantity sold
+SELECT 
+	p.product_name, 
+	SUM(CASE 
+			WHEN f.quantity IS NOT NULL THEN f.quantity
+			ELSE 0
+		END
+		) AS TotalQuantity
+FROM gold.fact_sales f
+RIGHT JOIN gold.dim_products p
+ON f.product_key = p.product_key
+GROUP BY p.product_name
+ORDER BY TotalQuantity ASC
+LIMIT 10;
+
 
 -- Top 5 countries by revenue
 
