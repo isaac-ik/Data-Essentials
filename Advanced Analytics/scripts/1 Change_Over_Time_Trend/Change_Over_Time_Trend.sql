62063c6a284aab6578e4a8f93c01cc0bdee25740
isaac-ik <isaac.ikpemi@gmail.com>
2026-01-23 09:28:32 -0500

    change over time - method 2

--- Advanced Analytics/scripts/1 Change_Over_Time_Trend/Change_Over_Time_Trend.sql
+++ Advanced Analytics/scripts/1 Change_Over_Time_Trend/Change_Over_Time_Trend.sql
@@ -1,26 +1,37 @@
-SELECT
-    DATE_TRUNC('month', f.order_date) AS month,
-    p.category,
-    SUM(f.sales_amount) AS total_sales
-FROM gold.fact_sales f
-JOIN gold.dim_products p
-    ON f.product_key = p.product_key
-GROUP BY
-    month,
-    p.category
-ORDER BY
-    month,
-    p.category;
-
-
-
-
+---- monthly sales trend by category -Method 1
 SELECT 
-	p.category, 
-	SUM(f.sales_amount), 
-	DATE_TRUNC('month', f.order_date) AS Month
-FROM gold.dim_products p
-LEFT JOIN gold.fact_sales f
-ON p.product_key = f.product_key
-GROUP BY month, category;
+	Category,
+	TotalSales,
+	MonthD,
+	RANK() OVER (PARTITION BY Category ORDER BY MonthD)
+FROM (
+	SELECT 
+			p.category, 
+			SUM(f.sales_amount) AS TotalSales, 
+			DATE_TRUNC('month', f.order_date) AS MonthD
+	FROM gold.dim_products p
+	LEFT JOIN gold.fact_sales f
+	ON p.product_key = f.product_key
+	GROUP BY monthD, category
+)
+WHERE monthD IS NOT NULL;
 
+----- Method 2
+
+SELECT
+    category,
+    monthd,
+    totalsales,
+    totalsales - LAG(totalsales) OVER (PARTITION BY category ORDER BY monthd)
+        AS mom_change
+FROM (
+    SELECT
+        p.category,
+        DATE_TRUNC('month', f.order_date) AS monthd,
+        SUM(f.sales_amount) AS totalsales
+    FROM gold.dim_products p
+    JOIN gold.fact_sales f
+        ON p.product_key = f.product_key
+    GROUP BY p.category, monthd
+) t
+ORDER BY category, monthd;
